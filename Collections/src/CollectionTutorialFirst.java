616be37923310ab66c046e631ae1688b60760809
mithu <mithu@DESKTOP-JJ4LF7U>
2020-02-01 15:19:26 -0500

    hi

---
+++ Collections/src/CollectionTutorialFirst.java
@@ -0,0 +1,27 @@
+
+public class CollectionTutorialFirst {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=25;
+		int c=a+b;
+		System.out.println(c);
+		xyz();
+		nyc();
+	}
+    public static void xyz() {
+	   int a = 45;
+	   int b = 56;
+	   int d =a-b;
+	   System.out.println(d);
+	   
+}
+    public static void nyc() {
+    	int y=100;
+    	String name="100";
+    	System.out.println(y+name);
+    }
+}
+
+		
